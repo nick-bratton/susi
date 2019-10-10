144385948a2480756b92b649d86a74068ac9158e
Nicholas Bratton <47389141+nick-bratton@users.noreply.github.com>
2019-10-10 16:04:28 +0200

    removes one stray console long

--- services/tenK.js
+++ services/tenK.js
@@ -34,7 +34,6 @@ let today = () => {
 	year = d.getFullYear();
 	if (month.length < 2){month = '0' + month};
 	if (day.length < 2){day = '0' + day};
-	console.log([year, month, day].join('-'));
 	return [year, month, day].join('-');
 }
 
