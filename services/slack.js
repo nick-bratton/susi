ebfa92ebb4bda1b94ad38408901f2b8fbbfbe32a
Nicholas Bratton <47389141+nick-bratton@users.noreply.github.com>
2020-02-03 11:18:02 +0100

    refactors slack.postMessageWithPayload and introduces Message class

--- services/slack.js
+++ services/slack.js
@@ -8,102 +8,49 @@ Slack = new WebClient(process.env.SLACK_PRO);
 
 const whitelist = require('../whitelist.js');
 
+class Message {
+	constructor(channel, payload){
+		this.message = {
+			"channel": channel,
+			"as_user": true,
+			"blocks": [
+				{
+					"type": "section",
+					"text": {
+						"type": "mrkdwn",
+						"text": "Please confirm your hours on 10000ft"
+					}
+				},
+				{
+					"type": "actions",
+					"block_id": "confirm_button",
+					"elements": [
+						{
+							"type": "button",
+							"text": {
+								"type":"plain_text",
+								"text": "Confirm Now"
+							},
+							"value": `${payload}`,
+							"action_id": "confirm_button_action_id"
+						}
+					]
+				}
+			]
+		}
+	}
+}
+
 const postMessageWithPayload = async(id, _payload) => {
 	try{
 		let payload = JSON.stringify(_payload);
-		if (process.env.MODE === 'dev') {
+		if (process.env.MODE === 'dev' || process.env.MODE === 'beta') {
 			if (whitelist.devEmail.includes(_payload.emailAddress)){
-				await Slack.chat.postMessage({
-					channel: `${id}`,
-					as_user: true,
-					"blocks": [
-						{
-							"type": "section",
-							"text": {
-								"type": "mrkdwn",
-								"text": "Please confirm your hours on 10000ft"
-							}
-						},
-						{
-							"type": "actions",
-							"block_id": "confirm_button",
-							"elements": [
-								{
-									"type": "button",
-									"text": {
-										"type":"plain_text",
-										"text": "Confirm Now"
-									},
-									"value": `${payload}`,
-									"action_id": "confirm_button_action_id"
-								}
-							]
-						}
-					]
-				});
-			}
-		}
-		else if (process.env.MODE === 'beta'){
-			if (whitelist.emails.includes(_payload.emailAddress)){
-				await Slack.chat.postMessage({
-					channel: `${id}`,
-					as_user: true,
-					"blocks": [
-						{
-							"type": "section",
-							"text": {
-								"type": "mrkdwn",
-								"text": "Please confirm your hours on 10000ft"
-							}
-						},
-						{
-							"type": "actions",
-							"block_id": "confirm_button",
-							"elements": [
-								{
-									"type": "button",
-									"text": {
-										"type":"plain_text",
-										"text": "Confirm Now"
-									},
-									"value": `${payload}`,
-									"action_id": "confirm_button_action_id"
-								}
-							]
-						}
-					]
-				});
+				await Slack.chat.postMessage(new Message(id, payload).message);
 			}
 		}
 		else if (process.env.MODE  === 'pro'){
-			await Slack.chat.postMessage({
-				channel: `${id}`,
-				as_user: true,
-				"blocks": [
-					{
-						"type": "section",
-						"text": {
-							"type": "mrkdwn",
-							"text": "Please confirm your hours on 10000ft"
-						}
-					},
-					{
-						"type": "actions",
-						"block_id": "confirm_button",
-						"elements": [
-							{
-								"type": "button",
-								"text": {
-									"type":"plain_text",
-									"text": "Confirm Now"
-								},
-								"value": `${payload}`,
-								"action_id": "confirm_button_action_id"
-							}
-						]
-					}
-				]
-			});
+			await Slack.chat.postMessage(new Message(id, payload).message);
 		}
 	}
 	catch(err){
@@ -111,23 +58,20 @@ const postMessageWithPayload = async(id, _payload) => {
 	}
 }
 
-exports.findAndMessageUser = (payload) => {
-	return new Promise(async function(resolve,reject){
-		await Slack.users.lookupByEmail({
-			email: `${payload.emailAddress}`
-		}).then(user => {
-			postMessageWithPayload(user.user.id, payload);
-			resolve(user.user.id);
-		})
-		.catch(err => {
-			console.log('Error in findAndMessageUser(): ' + err);
-		})
-	})
+exports.findAndMessageUser = async(payload) => {
+	try{
+		let user = await Slack.users.lookupByEmail({email: `${payload.emailAddress}`});
+		await postMessageWithPayload(user.user.id, payload);
+		return user.user.id;
+	}
+	catch(err){
+		throw err;
+	}
 }
 
 exports.getUserEmailAddressFromUserId = async(userId) => {
 	try{
-		let userInfo = await Slack.users.info({user: `${userId}`})
+		let userInfo = await Slack.users.info({user: `${userId}`});
 		return userInfo.user.profile.email;
 	}
 	catch(err){
