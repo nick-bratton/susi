91e9a67b53dcd753eab5e7584b2b55c93797e72e
Nicholas Bratton <47389141+nick-bratton@users.noreply.github.com>
2020-02-03 17:31:33 +0100

    refactors app.js for readability

--- app.js
+++ app.js
@@ -11,23 +11,15 @@ const slack = require('./services/slack.js');
 let interval = '0 10 * * MON';
 
 const main = async() => {
-	let allWeeklyEntries = await tenK.getWeeklyEntries();
-	let unconfirmedEntryIdentifiers = await tenK.getUnconfirmedEntryIdentifiers(allWeeklyEntries);
-	let messagePayloads = await tenK.constructPayloads(allWeeklyEntries, unconfirmedEntryIdentifiers);
-	messageContacts(messagePayloads);
-}
-
-const messageContacts = async(payloads) => {
-	await Promise.all(payloads.map(payload => slack.findAndMessageUser(payload)))
-		.then(slackUserIds => {
-			console.log('Notified Slack users: ' + slackUserIds);
-		})
-		.catch(err => {
-			console.log('Error in notifyContacts(): ' + err)
-		})
-		.finally(function(){
-			console.log('Done.');
-		});
+	try{
+		let allWeeklyEntries = await tenK.getWeeklyEntries();
+		let unconfirmedEntryIdentifiers = await tenK.getUnconfirmedEntryIdentifiers(allWeeklyEntries);
+		let payloads = await tenK.constructPayloads(allWeeklyEntries, unconfirmedEntryIdentifiers);
+		await Promise.all(payloads.map(payload => slack.findAndMessageUser(payload)))
+	}
+	catch(err){
+		throw err;
+	}
 }
 
 // new Cron(interval, function() {
@@ -35,6 +27,3 @@ const messageContacts = async(payloads) => {
 // }, null, true, 'Europe/Berlin');
 
 main()
-	.catch(err => {
-		throw err;
-	})
