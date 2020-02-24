c7142e82a3773109cb60e119d49a233ffb0a4ab7
Nicholas Bratton <47389141+nick-bratton@users.noreply.github.com>
2020-02-24 12:02:12 +0100

    adds some tests and groups by describe to __tests__/mongo

--- __tests__/mongo.js
+++ __tests__/mongo.js
@@ -15,26 +15,53 @@ let submissionPayload = {
 }
 
 
-it('should return a Message document with 4 props of some type', () => {
-  let doc = new mongo.Message(messagePayload)
-  expect(doc.document).toMatchObject({
-    date: expect.any(String),
-    usersMessaged: expect.anything(),
-    totalUsers: expect.anything(),
-    messages: expect.any(Array)
+describe('Message', () => {
+
+  it('should return a Message document with 4 props of some type', () => {
+    let doc = new mongo.Message(messagePayload)
+    expect(doc.document).toMatchObject({
+      date: expect.any(String),
+      usersMessaged: expect.anything(),
+      totalUsers: expect.anything(),
+      messages: expect.any(Array)
+    })
+  })
+
+  it('length of this.messages should be length of this.amtUsersMessaged', () => {
+    let p = {
+      messages: new Array(10),
+      metadata: {
+        usersMessaged: 10,
+        totalUsers: 20
+      }
+    };
+    let doc = new mongo.Message(p);
+    expect(doc.document.usersMessaged).toBe(p.messages.length);
   })
 })
 
-it('should return a Submission document with 3 props of some type', () => {
-  let doc = new mongo.Submission(submissionPayload);
-  expect(doc.document).toMatchObject({
-    date: expect.any(String),
-    payload: expect.anything(),
-    error: undefined
-  });
+
+
+describe('Submission', () => {
+
+  it('should return a Submission document with 3 props of some type', () => {
+    let doc = new mongo.Submission(submissionPayload.payload, submissionPayload.error);
+    expect(doc.document).toMatchObject({
+      date: expect.any(String),
+      payload: expect.anything(),
+      error: undefined
+    });
+  })
+  
+  it('should return a Submission document with a defined error', ( ) => {
+    let payload = submissionPayload;
+    payload.error = 'Some error';
+    let doc = new mongo.Submission(payload, payload.error);
+    expect(doc.document).toMatchObject({
+      date: expect.any(String),
+      payload: expect.anything(),
+      error: `${payload.error}`
+    });
+  })
+
 })
-
-// it('should return a Submission document with a defined error', ( ) => {
-
-//   expect();
-// })
